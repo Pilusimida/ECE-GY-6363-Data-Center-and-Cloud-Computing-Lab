52e1b76fc448db84c88f59936cbe65c186747ce5
Pilusimida <84691256+Pilusimida@users.noreply.github.com>
2025-03-03 19:12:38 -0500

    Update fat_tree.py

--- fat_tree.py
+++ fat_tree.py
@@ -1,13 +1,26 @@
+#!/usr/bin/python
+
+"""
+This setup the topology in lab3-part3
+"""
+
 from mininet.topo import Topo
 from mininet.net import Mininet
+from mininet.node import Controller, RemoteController
 from mininet.cli import CLI
-from mininet.log import setLogLevel
-from mininet.node import RemoteController
-
+from mininet.log import setLogLevel, info
+from mininet.util import dumpNodeConnections
+from mininet.link import Link, Intf, TCLink
+import os 
+from time import sleep
+import sys
 N = 4 
 
-class FatTreeTopo(Topo):
+class Topology(Topo):
+    
+    
     def __init__(self):
+        "Create Topology." 
         Topo.__init__(self)
 
         core_switches = []
@@ -29,14 +42,36 @@ class FatTreeTopo(Topo):
                 host = self.addHost(f'h{i*N//2+j+1}')
                 self.addLink(host, edge_switches[i])
 
-def run_experiment():
-    topo = FatTreeTopo()
-    ctrl = RemoteController('ryu', ip='127.0.0.1', port=6633)
-    net = Mininet(topo=topo, controller=ctrl)
+# This is for "mn --custom"
+topos = { 'mytopo': ( lambda: Topology() ) }
+
+# This is for "python *.py"
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+            
+    topo = Topology()
+    net = Mininet(topo=topo, link=TCLink)       # The TCLink is a special setting for setting the bandwidth in the future.
+    
+    # 1. Start mininet
     net.start()
-    CLI(net)
+    
+    
+    # Wait for links setup (sometimes, it takes some time to setup, so wait for a while before mininet starts)
+    print "\nWaiting for links to setup . . . .",
+    sys.stdout.flush()
+    for time_idx in range(3):
+        print ".",
+        sys.stdout.flush()
+        sleep(1)
+    
+        
+    # 2. Start the CLI commands
+    info( '\n*** Running CLI\n' )
+    CLI( net )
+    
+    
+    # 3. Stop mininet properly
     net.stop()
 
-if __name__ == '__main__':
-    setLogLevel('info')
-    run_experiment()
+
+    ### If you did not close the mininet, please run "mn -c" to clean up and re-run the mininet 
